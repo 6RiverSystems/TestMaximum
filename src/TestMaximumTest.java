c583d76370963b583f5cf105304a4a2b4c80d206
Jeevanbs <41687022+Jeevanbs@users.noreply.github.com>
2021-04-06 17:52:29 +0530

    TestMaximum - UC1 All Test Cases

--- src/TestMaximumTest.java
+++ src/TestMaximumTest.java
@@ -5,10 +5,40 @@ import org.junit.Test;
 public class TestMaximumTest {
 
 	@Test
-	public void testCompfunc() {
+	public void testCompfuncUc1TT1() {
 		Integer n1 = 3;
 		Integer n2 = 2;
 		Integer n3 = 1;
+		
+		Integer expectedResult = 3;
+		try {
+			Integer actualResult = TestMaximum.class.newInstance().compfunc(n1, n2, n3);
+			assertEquals(expectedResult, actualResult);
+		} catch (InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testCompfuncUc1TT2() {
+		Integer n1 = 2;
+		Integer n2 = 3;
+		Integer n3 = 1;
+		
+		Integer expectedResult = 3;
+		try {
+			Integer actualResult = TestMaximum.class.newInstance().compfunc(n1, n2, n3);
+			assertEquals(expectedResult, actualResult);
+		} catch (InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testCompfuncUc1TT3() {
+		Integer n1 = 1;
+		Integer n2 = 2;
+		Integer n3 = 3;
 		
 		Integer expectedResult = 3;
 		try {
